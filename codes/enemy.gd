216150a63a382f7d75304f157b95d94caa7f5bfb
snowduck07 <hassedominik@gmail.com>
2024-06-30 21:17:35 +0200

    30.06. 21:16
    irgendwo im combat system m17 Problem: sprite außerhalb der hitbox beim nach links Laufen

--- codes/enemy.gd
+++ codes/enemy.gd
@@ -4,7 +4,8 @@ var speed = 200
 var chase = false
 var player = null
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var health = 100
+var player_in_range = false
 
 
 func _physics_process(delta):
@@ -16,7 +17,10 @@ func _physics_process(delta):
 		position += (player.position - position).normalized() * speed * delta
 		move_and_slide() 
 
+	if health <= 0:
+		self.queue_free()
 
+	deal_with_damage()
 
 func _on_detection_area_body_entered(body):
 	player = body
@@ -28,3 +32,26 @@ func _on_detection_area_body_exited(body):
 
 func enemy():
 	pass
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_in_range = true
+
+func _on_enemy_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_in_range = false
+
+func deal_with_damage():
+	if player_in_range and Global.player_attack == true:
+		health = health - 25
+		print("enemy health:" + health)
+
+
+
+
+
+
+
+
+
--- codes/player.gd
+++ codes/player.gd
@@ -4,40 +4,49 @@ var enemy_in_range = false
 var enemy_cooldown = true
 var health = 100 
 var alive = true
-
+var attack_ip = false
 
 const SPEED = 380.0
 const JUMP_VELOCITY = -450.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("walk_left", "walk_right")
+
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("run")
+		if direction == -1:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("idle")
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		$AnimatedSprite2D.play("jump")
 
 	move_and_slide()
 	enemy_attack()
 
-func _on_player_hitbox_body_entered(body):
+	if health <= 0:
+		health = 0
+		get_tree().change_scene_to_file("res://scenes/deathscreen.tscn")
 
+func _on_player_hitbox_body_entered(body):
 	if body.has_method("enemy"):
 		enemy_in_range = true
-
 
 func _on_player_hitbox_body_exited(body):
 	if body.has_method("enemy"):
@@ -45,11 +54,13 @@ func _on_player_hitbox_body_exited(body):
 
 func enemy_attack():
 	if enemy_in_range and enemy_cooldown:
-		health = health - 10
+		health = health - 20
 		enemy_cooldown = false
 		$cooldown.start()
+		print(health)
+
+func _on_cooldown_timeout():
+	enemy_cooldown = true
 
 func player():
 	pass
-
-
